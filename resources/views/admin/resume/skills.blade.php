1254297529fa71d73d5e9906936707d527503c4b
randomAccessMe <ramaleh91@gmail.com>
2016-06-21 16:35:12 -0400

    refactor javascript in the admin area

--- resources/views/admin/resume/skills.blade.php
+++ resources/views/admin/resume/skills.blade.php
@@ -50,33 +50,5 @@
     $('#form-container').on('click', '.delete-skill', function() {
         deleteInstance($(this), 'skill');
     });
-
-    function freshDuplicate(element, elementName, Callback) {
-
-        element.data('iterator', element.data('iterator') + 1);
-        var iteration = element.data('iterator');
-        var clone = $('.' + elementName + '-form-fields').first().clone();
-        clone.find('input, textarea').each(function () {
-            var fieldName = $(this).attr('name');
-            $(this).val(null).attr({
-                name: fieldName.replace(/[a-z]{3}\[\d\]/i, 'new['+iteration+']')
-            });
-        });
-        Callback(clone);
-        clone.find('.delete-' + elementName + '').removeAttr('href')
-        clone.appendTo('#form-container');
-    }
-
-    function deleteInstance(instanceElement, instanceName) {
-        if($('.' + instanceName + '-form-fields').length <= 1) {
-            alert('This is the last ' + instanceName.capitalizeFirstLetter() + '!');
-            return;
-        }
-        instanceElement.parents('.' + instanceName + '-form-fields').remove();
-    }
-
-    String.prototype.capitalizeFirstLetter = function() {
-        return this.charAt(0).toUpperCase() + this.slice(1);
-    }
 </script>
 @endpush
